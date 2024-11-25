ceb4c5f46eb74c69af3efd991720b984ddc69df0
leonardo-31415 <119406759+leonardo-31415@users.noreply.github.com>
2024-11-25 13:25:53 +0100

    Update openlime.js
    Modificato RBF-stress

--- Palinsesto/CPVRm0040_1_220v223r/js/openlime.js
+++ Palinsesto/CPVRm0040_1_220v223r/js/openlime.js
@@ -8976,7 +8976,8 @@ color = vec4(vec3(dot(light, normal)), 1);
 
 			if (this.albedo == true && this.mode == 'stress')
 				str += `
-	color = color - texture(albedo, v_texcoord) + texture(stress, v_texcoord);
+	vec3 n = texture(normals,v_texcoord).xyz * 2.0 - 1.0;
+	color = color + dot(light,n) * (- texture(albedo, v_texcoord) + texture(stress, v_texcoord));
 `;
     		str += `
 	return color;
